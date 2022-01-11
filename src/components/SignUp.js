462d2e295647faa6b912fcc759f7b74124c3887e
Anderson Luis Silva <andersonluis87@gmail.com>
2022-01-11 18:12:38 -0300

    FIx Password Match Message

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -28,7 +28,7 @@ export const SignUp = () => {
             .min(8, 'Must be at least 8 characters')
             .required('Password is required'),
         confirmPassword: Yup.string()
-            .oneOf([Yup.ref('password'), null, 'Passwords must match'])
+            .oneOf([Yup.ref('password')], null, 'Passwords must match')
             .required('Confirm password is required'),
         gender: Yup.string().required('Gender is required'),
         terms: Yup.boolean()
